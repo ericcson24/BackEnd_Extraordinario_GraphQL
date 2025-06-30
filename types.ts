933d13ddb70793b162ab6247b9f6e9b09b0a4067
ericcson24 <150656730+ericcson24@users.noreply.github.com>
2025-06-30 11:01:15 +0200

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,21 +1,3 @@
 import { OptionalId } from "mongodb";
 
 
-export type Usuario=OptionalId<{
-    name:string,
-    phone:string,
-   
-    direction:string,
-    city:string,
-    country:string
-}>
-
-export type API_telefono={
-    is_valid:boolean,
-    country:string
-  
-}
-
-export type API_correo={
-    is_valid:boolean
-}
