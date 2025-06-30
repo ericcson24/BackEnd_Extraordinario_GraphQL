b512eb0458a49e4f0491caf3c84693f62722edbc
ericcson24 <150656730+ericcson24@users.noreply.github.com>
2025-06-30 16:19:03 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-// deno-lint-ignore-file
-import { MongoClient } from 'mongodb'
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from "./typeDefs.ts";
-import { resolvers } from "./resolvers.ts";
-import { Usuario } from "./types.ts";
-
+//deno main.ts
